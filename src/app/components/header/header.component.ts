3cbf97a7249f904ed43ebdc6f57aab638cd5e8f6
mijail cohen <mijaelcohen@gmail.com>
2021-08-17 17:13:20 -0300

    moved lang select to navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
     selector: 'app-header',
@@ -6,4 +7,22 @@ import { Component } from "@angular/core";
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+    
+    public defaultLang : string  = 'en';
+    public currentLang : string;
+    public supportedLangs : Array<string> = ['en', 'es'];
+
+    constructor(public translate : TranslateService){
+        translate.addLangs(this.supportedLangs);
+        translate.setDefaultLang(this.defaultLang);
+  
+        const browserLang = translate.getBrowserLang();
+        this.currentLang = browserLang.match(/en|es/) ? browserLang : 'en'
+        translate.use(this.currentLang);
+    }
+    public changeLang(lang: string){
+        this.translate.use(lang);
+        this.currentLang = lang
+    }
+    
 }
